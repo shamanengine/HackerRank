73fb5f2c0366e26d77b036538a2cf7df5f3b213d
atymchenko <artem.tym@gmail.com>
2019-01-26 16:51:35 +0100

    validating postal code

--- src/13_regex_and_parsing/validating_postalcode.py
+++ src/13_regex_and_parsing/validating_postalcode.py
@@ -13,16 +13,26 @@ Sample Output 0
 False
 '''
 
-regex_integer_in_range = r"[1-9][0-9]{5}"  # Do not delete 'r'.
-regex_alternating_repetitive_digit_pair = r"^.*(.).\1"  # Do not delete 'r'.
+regex_integer_in_range = r"^[1-9][0-9]{5}$"  # Do not delete 'r'.
+regex_alternating_repetitive_digit_pair = r"(.)(?=.\1)"  # Do not delete 'r'.
 
 import re
 
 P = input()
+# P = '4542867'
 
 print(bool(re.match(regex_integer_in_range, P))
       and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
 
+# print(re.match(regex_integer_in_range, P))
+
+'''
+P = '110000'
+print(re.search(regex_alternating_repetitive_digit_pair, P))
 print(bool(re.search(regex_alternating_repetitive_digit_pair, P)))
 
+for item in re.findall(regex_alternating_repetitive_digit_pair, P):
+    print(item)
+
 print(len(re.findall(regex_alternating_repetitive_digit_pair, P)))
+'''
