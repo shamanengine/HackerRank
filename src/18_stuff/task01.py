befcc816e4b5960eb183d49e21d39117331c7001
artem_tym <artem.tym@gmail.com>
2019-02-10 17:43:10 +0100

    some tasks for stuff

--- src/18_stuff/task01.py
+++ src/18_stuff/task01.py
@@ -3,12 +3,14 @@ Sum all 2-digit numbers from the sequence
 '''
 
 arr = list(map(int, input().strip().split()))
-print(sum([x for x in arr if (10 <= x <= 99)]))
+print(sum([x for x in arr if (10 <= x <= 99) or (-99 <= x <= -10)]))
 
 '''
 Input
 1 2 3 11 322 20 9989 99
+1 2 3 11 322 20 9989 -99
 
 Output 
 130
+-68
 '''
